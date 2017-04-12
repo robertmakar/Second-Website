d3be0a31ccb94b5cf741e364710d72c92a5f1ad7
Robert <robert.ashraf@hotmail.com>
2017-04-12 23:48:49 +0200

    Added Bootstrap files, started jQuery implementation for basic animations.

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('footer img').hover(function(){
+    $(this).animate({width:'60px', height:'60px'}, 'fast');
+  }, function(){
+    $(this).animate({width:'50px', height:'50px'}, 'fast');
+  });
+});
