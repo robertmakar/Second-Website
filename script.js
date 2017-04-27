74cfa1f0c7dfe4158ed8713513054aa1cc3d58c3
Robert <robert.ashraf@hotmail.com>
2017-04-27 10:07:42 +0200

    updated

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+/*$(function() {
+var body = $(‘.header’);
+var backgrounds = new Array(
+‘url(images/image1.jpg)’,
+‘url(images/image2.jpg)’
+);
+var current = 0;
+
+function nextBackground() {
+.header.css(
+‘background’,
+backgrounds[current = ++current % backgrounds.length]
+);
+
+setTimeout(nextBackground, 10000);
+}
+setTimeout(nextBackground, 10000);
+.header.css(‘background’, backgrounds[0]);
+});
+
+
+window.onload=function(){
+$(".header").fadeOut(4000);
+$(".header").addClass("zoom");
+    setTimeout(function(){
+    $(".header").removeClass("zoom");
+    },5000);
+}
 /*$(document).ready(function(){
   $('footer a img').hover(function(){
     $(this).animate({width:'55px', height:'55px'}, 'fast');
