18223e928adc76dbea632974e7b400fd79f93387
Robert <robert.ashraf@hotmail.com>
2017-04-27 09:13:51 +0200

    updated the site

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*$(document).ready(function(){
-  $('footer img').hover(function(){
-    $(this).animate({width:'60px', height:'60px'}, 'fast');
-  }, function(){
+  $('footer a img').hover(function(){
+    $(this).animate({width:'55px', height:'55px'}, 'fast');
+      }, function(){
     $(this).animate({width:'50px', height:'50px'}, 'fast');
   });
 });
