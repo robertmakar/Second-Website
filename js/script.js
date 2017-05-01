2f52dcce9895ce041b805f5e94c378e48f5c40b6
Robert <robert.ashraf@hotmail.com>
2017-05-01 05:19:04 +0300

    Active class, section dividers and new images.
    Added dividers between sections, fixed bg attachment for the pixel
    section, added 2 new pictures to the slider and added the active class
    for the nav bar

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 $(document).ready(function(){
+  //Social icons Animations
   $('.footer img').hover(function(){
     $(this).animate({width:'55px', height:'55px'}, 'fast');
       }, function(){
     $(this).animate({width:'50px', height:'50px'}, 'fast');
   });
 
+  //Pixel Bubble Appearance
   $('.pixel').hover(function(){
     $(".bubble").animate({opacity:'1'}, 'fast');
   });
 
+  //Slider controls
 	$('.my-slider').unslider({
     infinite: true,
     autoplay: true,
+    delay:5000,
     arrows: false,
     nav: false
   });
 
+  //Adding Active class to the active section in navi bar
+  $('.navigationbar ul li a').click(function(){
+    $('li a').removeClass("active");
+    $(this).addClass("active");
+});
+
+  //Anchor scrolling animations
   var $root = $('html, body');
   $('.anchor').click(function() {
     $root.animate({
